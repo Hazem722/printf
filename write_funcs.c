9eba8f21dc4ba5e2fdeaca9ddf4e2a8beb5fe905
Hazem722 <hazemba684@gmail.com>
2022-08-06 08:41:19 -0700

    printf func

---
+++ write_funcs.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - ...
+ * @c:
+ * Return: 1 (Success)
+ */
+int _putchar(char c)
+{
+	static char buf[1024];
+	static int i;
+
+	if (c == -1 || i >= 1024)
+	{
+		write(1, &buf, i);
+		i = 0;
+	}
+	if (c != -1)
+	{
+		buf[i] = c;
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * _puts - ...
+ * @str: ...
+ * Return:.....
+ */
+int _puts(char *str)
+{
+	register int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	return (i);
+}
