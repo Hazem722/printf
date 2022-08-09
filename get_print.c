dd344a0c2f70a31203d3b6747de0c32aaad4cae3
Ivan Epou <ivanepou92@gmail.com>
2022-08-09 01:40:27 -0700

    Add functions

--- get_print.c
+++ get_print.c
@@ -18,10 +18,14 @@ int (*get_print(char s))(va_list, flags_t *)
 		{'s', print_string},
 		{'c', print_char},
 		{'%', print_percent},
-		{'b', print_binary}
+		{'b', print_binary},
+		{'u', print_unsigned},
+		{'x', print_hex},
+		{'X', print_hex_big},
+		{'o', print_octal}
 		};
 
-	int flags = 6;
+	int flags = 10;
 
 	register int i;
 
